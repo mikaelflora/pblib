4579f39635fdff883ba912e9603a918cc9fab378
mikaelflora <mikaelflora@hotmail.com>
2016-06-17 13:46:11 +0200

    [Feature] Update iniparsing.bash
    get keys

--- pblib/iniparsing.bash
+++ pblib/iniparsing.bash
@@ -3,12 +3,85 @@
 
 # ----------------------------------------------------------------------------
 # :author:  Mikael FLORA <mikaelflora@hotmail.com>
-# :version: 0.1.2
-# :date:    2016-06-16
+# :version: 0.2
+# :date:    2016-06-17
 # :brief:   config parser (ini file) with associative array
-# :TODO:    code review ini.common.to.Array and ini.to.Array
+# :TODO:    code review
 # ----------------------------------------------------------------------------
 
+
+
+ini.common.to.Array.key () {
+# desc: load key's value from config gile section to key associative array
+# $1: file to parse
+# $2: associtive array name to use
+# $3: section to load
+# $4: key to get
+  tmp=${2}[${4}]
+  if [ ! ${!tmp+a} ]; then
+    return 1
+  fi
+  unset tmp
+
+  while read line; do
+    if [[ $line == ?*"="* ]]; then
+      key=$(echo "${line%%=*}" | sed 's/[ \t]*//g')
+      if [ "$4" = "$key" ]; then
+        value=$(echo "${line##*=}" | sed 's/^[ \t]*//;s/[ \t]*$//')
+        tmp=${2}[${key}]
+        if [ ${!tmp+a} ]; then
+          eval ${2}[\${key}]=\${value}
+        fi  
+        unset value tmp
+      fi
+      unset key
+    fi  
+  done < <(sed -e '/\[.*\]/,$d' ${1} | sed "s#;.*##g")
+  unset tmp key value
+}
+
+
+ini.to.Array.key () {
+# desc: load key's value from config gile section to key associative array
+# $1: file to parse
+# $2: associtive array name to use
+# $3: section to load
+# $4: key to get value
+  tmp=${2}[${4}]
+  if [ ! ${!tmp+a} ]; then
+    return 1
+  fi
+  unset tmp
+
+  ini.common.to.Array.key $1 $2 $3 $4
+
+  while read line; do
+    if [[ $line == ?*"="* ]]; then
+      key=$(echo "${line%%=*}" | sed 's/[ \t]*//g')
+      if [ "$4" = "$key" ]; then
+        value=$(echo "${line##*=}" | sed 's/^[ \t]*//;s/[ \t]*$//')
+        tmp=${2}[${key}]
+        if [ ${!tmp+a} ]; then
+          eval ${2}[\${key}]=\${value}
+        fi  
+        unset value tmp
+      fi
+      unset key
+    fi  
+  done < <(sed -n "/\[[ \t]*${section}[ \t]*\]/{:a;n;/^$/b;p;ba}" ${1} | sed "s#;.*##g")
+  unset tmp key value
+}
+
+
+ini.to.Array.keys () {
+# desc: load config from sections in file to associative arrays
+# $1: file to parse 
+# $2: sections / associative arrays to load / use
+# ${*:3}: keys to get values
+  for key in ${*:3}; do
+    ini.to.Array $1 $2 $2 ${key}
+  done
+}
 
 
 ini.common.to.Array () {
@@ -16,15 +89,14 @@ ini.common.to.Array () {
 # $1: file to parse
 # $2: associative array name to use
   while read line; do
-    #if [[ $line == ?*"="?* ]]; then
-    if [[ $line == ?*"=" ]]; then
+    if [[ $line == ?*"="* ]]; then
       key=$(echo "${line%%=*}" | sed 's/[ \t]*//g')
-        value=$(echo "${line##*=}" | sed 's/^[ \t]*//;s/[ \t]*$//')
-        tmp=${2}[${key}]
-        if [ ${!tmp+a} ]; then
-          eval ${2}[\${key}]=\${value}
-        fi  
-        unset key value tmp
+      value=$(echo "${line##*=}" | sed 's/^[ \t]*//;s/[ \t]*$//')
+      tmp=${2}[${key}]
+      if [ ${!tmp+a} ]; then
+        eval ${2}[\${key}]=\${value}
+      fi  
+      unset key value tmp
     fi
   done < <(sed -e '/\[.*\]/,$d' ${1} | sed "s#;.*##g")
 }
@@ -41,8 +113,7 @@ ini.to.Array () {
 
   if [ -f ${1} ]; then
     while read line; do
-      #if [[ $line == ?*"="?* ]]; then
-      if [[ $line == ?*"=" ]]; then
+      if [[ $line == ?*"="* ]]; then
         key=$(echo "${line%%=*}" | sed 's/[ \t]*//g')
         value=$(echo "${line##*=}" | sed 's/^[ \t]*//;s/[ \t]*$//')
         tmp=${2}[${key}]
@@ -121,51 +192,28 @@ alias iniparse.write='Arrays.to.ini'
 alias iniparse.load.as='ini.to.Array'
 alias iniparse.print.as='print.Array'
 alias iniparse.write.as='Array.to.ini'
+alias iniparse.get.key='ini.to.Array.key'
+alias iniparse.get.keys='ini.to.Array.keys'
 
 
 if [ "${BASH_SOURCE##*/}" = "${0##*/}" ]; then
-  declare -A CONFIG=(
-    [f]="foo"
-    [b]="bar"
-    [t]=""
-  )
-
   declare -A CONF=(
-    [a]="bar"
-    [b]="foo"
+    [b]="bar"
+    [f]=""
   )
 
-  echo "print associative arrays:"
-  iniparse.print CONF CONFIG
-  echo "write test.conf,"
-  iniparse.write test.conf CONF CONFIG
-  echo "and cat test.conf:"
+  echo "print associative array:"
+  iniparse.print CONF
+  echo "write test.conf and cat test.conf:"
+  iniparse.write test.conf CONF
   cat test.conf
-  echo "unset CONF[b],"
-#  unset CONF[b]
-  echo "modify CONFIG[b]=Hello World,"
-  CONFIG[b]="Hello World"
-  echo "modify CONFIG[t]=Hello World,"
-  CONFIG[t]="Hello World"
-  echo "and print associative arrays:"
-  iniparse.print CONF CONFIG
-  echo "load test.conf in associative arrays,"
-  iniparse.load test.conf CONF CONFIG
-  echo "and print associative arrays:"
-  iniparse.print CONF CONFIG
+  echo "unset CONF[b] and print associative array:"
+  unset CONF[b]
+  iniparse.print CONF
+  echo "load test.conf in associative array,"
+  iniparse.load test.conf CONF
+  echo "and print associative array:"
+  iniparse.print CONF
   echo "rm test.conf"
   rm test.conf
-
-  unset $key $value
-  echo "print not void values in CONFIG:"
-  for key in ${!CONFIG[@]}; do
-    if [[ ${CONFIG[${key}]} ]]; then
-      echo "$value=$key"
-    fi
-  done
-  unset $key $value
-  echo "print set key in CONFIG:"
-  for key in ${!CONFIG[@]}; do
-    test ${CONFIG[${key}]+a} && echo "$value=$key"
-  done
 fi
